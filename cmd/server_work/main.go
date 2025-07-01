9c25400e36da788d44340704ffc62ffbbcd9e468
duebchy <vdovinkirill609@gmail.com>
2025-07-01 22:37:04 +0600

    random messages adding to file

--- cmd/server_work/main.go
+++ cmd/server_work/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"gRPCqueue/messagepb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-
 	"math/rand"
+	"os"
+	"time"
+
 	"strconv"
 )
 
@@ -22,7 +23,7 @@ func WriteMessage(messageClient messagepb.MsgServiceClient, content string, Idem
 
 	return out.ID, err
 }
-func GetMessageByOffser(messageClient messagepb.MsgServiceClient, Offset uint32, Limit uint32) ([]string, error) {
+func GetMessageByOffset(messageClient messagepb.MsgServiceClient, Offset uint32, Limit uint32) ([]string, error) {
 	out, err := messageClient.GetMessageByOffset(context.Background(), &messagepb.GetMessageByOffsetRequest{
 		Offset: Offset,
 		Limit:  Limit,
@@ -32,20 +33,44 @@ func GetMessageByOffser(messageClient messagepb.MsgServiceClient, Offset uint32,
 	}
 	return ([]string)(out.Messages), nil
 }
+func getMessageByID(messageClient messagepb.MsgServiceClient, Id string) (string, error) {
+	out, err := getMessageByID(messageClient, Id)
+	if err != nil {
+		return "", err
+	}
+	return (string)(out), nil
+}
+
 func main() {
 	newClient, err := grpc.NewClient("localhost:1488", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		panic(err)
 	}
-
+	idx_of_msg := 0
 	messageClient := messagepb.NewMsgServiceClient(newClient)
-	for i := 0; i < 8; i++ {
-		ID, err := WriteMessage(messageClient, "yo"+strconv.Itoa(i), "2007"+strconv.Itoa(rand.Intn(100000000000)))
+	file, err := os.Create("cmd/server_work/log/" + "message_" + strconv.Itoa(idx_of_msg) + "-" + strconv.Itoa(idx_of_msg+100) + ".txt")
+	if err != nil {
+		panic(err)
+	}
+	var content string
+	var idempotencyKey string
+
+	for {
+		cnt_for_new_file := 0
+		content = "number: " + strconv.Itoa(rand.Intn(100000))
+		idempotencyKey = "test" + strconv.Itoa(rand.Intn(100000))
+		out, err := WriteMessage(messageClient, content, idempotencyKey)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println("ID:", ID)
+		file.WriteString(out + " " + content + " " + idempotencyKey + " idx: " + strconv.Itoa(idx_of_msg) + "\n")
+		cnt_for_new_file++
+		idx_of_msg += 1
+		if cnt_for_new_file == 100 {
+			file.Close()
+			break
+		}
+
+		time.Sleep(2 * time.Second)
 	}
-	out, err := GetMessageByOffser(messageClient, 0, 5)
-	fmt.Println("offset out: ", out)
 }
